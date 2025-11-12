47f64e9e662564b83a4104fe64d226b906c3757d
Jisoo Lee <jisoo.h.lee@gmail.com>
2025-11-12 20:44:16 +0900

    change cwd as current buffer's path

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,8 +1,17 @@
--- Autocmds are automatically loaded on the VeryLazy event
--- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
---
--- Add any additional autocmds here
--- with `vim.api.nvim_create_autocmd`
---
--- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
--- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+--- Autocmds are automatically loaded on the VeryLazy event
+--- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
+---
+--- Add any additional autocmds here
+--- with `vim.api.nvim_create_autocmd`
+---
+--- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
+--- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Auto change directory to current buffer's directory
+vim.api.nvim_create_autocmd("BufEnter", {
+  pattern = "*",
+  callback = function()
+    vim.cmd("lcd %:p:h")
+  end,
+  desc = "Auto change local directory to buffer's directory",
+})
